f7b0d46eb85e455e988a7caa801a761002877e7f
Michael Pope <popemray1986@gmail.com>
2018-06-10 18:41:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(drivers, name) {
   const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'
-  for (const driver of collection) {
+  for (const driver of drivers) {
     return driver.name;
   }
 }

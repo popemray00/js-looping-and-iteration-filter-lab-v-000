80c36fcfdea3789343d6320fe0e2fa56d4d7334d
Michael Pope <popemray1986@gmail.com>
2018-06-10 18:50:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 // Code your solution in this file
 function findMatching(arr, str) {
-  return arr.filter(function(name) {
+   arr.filter(function(name) {
     return name.toLowerCase() === str.toLowerCase()
   })
 }
 
 function fuzzyMatch(arr, letter) {
-  return arr.filter(function(name) {
-    return name.charAt(0) === letter.charAt(0)
-  })
-}
-
-function matchName(arr, str) {
-  return arr.filter(function(name) {
-    return name.name.toLowerCase() === str.toLowerCase()
-  })
-}function fuzzyMatch(arr, letter) {
   return arr.filter(function(name) {
     return name.charAt(0) === letter.charAt(0)
   })

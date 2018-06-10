1a0142aaada4ca0e2c08d830acb1ac62643d7c25
Michael Pope <popemray1986@gmail.com>
2018-06-10 18:34:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
 function findMatching(drivers, name) {
   for (const driver of drivers) {
     if (driver.name === name) {
-    return driver.name;
+    return driver.name.toUpperCase();
     }
   }
-  
+
 }

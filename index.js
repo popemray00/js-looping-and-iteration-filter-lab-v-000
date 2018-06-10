c824c610c2b4f20374d6a9f38c047fc656b4e050
Michael Pope <popemray1986@gmail.com>
2018-06-10 18:46:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 // Code your solution in this file
-function findMatching(drivers, name) {
-  const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
-  for (const driver of drivers) {
-    return driver.name;
-  }
+function findMatching(arr, str) {
+  return arr.filter(function(name) {
+    return name.toLowerCase() === str.toLowerCase()
+  })
 }
